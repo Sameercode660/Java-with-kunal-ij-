6f9ee3aba7317c7776002387450c9702d77e3267
Mohd Sameer <mesh789736@gmail.com>
2024-09-19 12:35:44 +0530

    completed super class concept

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -1,4 +1,57 @@
+
+
+
+
 public class Inheritance
 {
+    public static void main(String[] args) {
+        Sample smp1 = new Sample(9, 9, 9, 9);
+
+        smp1.display();
+
+    }
+}
+
+class Area {
+
+    int length;
+    int width;
+    int radius;
+
+    Area() {
+
+    }
+    Area(int length, int width, int radius) {
+        this.length = length;
+        this.width = width;
+        this.radius = radius;
+    }
+
+    void display() {
+        System.out.println("Length is : "+ this.length);
+        System.out.println("Width is : "+ this.width);
+        System.out.println("Radius is : "+ this.radius);
+    }
+}
+
+class Sample extends  Area {
+
+    private  int  num ;
+
+    Sample(int length, int width, int radius, int num) {
+        super(length, width, radius);
+        this.num = num;
+    }
+
+    void display() {
+
+        System.out.println("Length is : "+ this.length);
+        System.out.println("Width is : "+ this.width);
+        System.out.println("Radius is : "+ this.radius);
+        System.out.println("Num is: " + this.num);
+    }
+
 
 }
+
+
